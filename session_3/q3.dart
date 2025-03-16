fe2872aa1466977631ad55eae9e81d5baa64e63e
rabbaaaaaa <radwaali012345@gmail.com>
2025-03-16 21:54:11 +0200

    Done

--- session_3/q3.dart
+++ session_3/q3.dart
@@ -1,3 +1,22 @@
 /* Create a to-do list program using a List where each item has a description, due date, and
  completion status (as bool). Implement methods to add, remove, and update tasks, including the
  use of for-each loops*/
+void main() {
+  toDoList task1 = toDoList();
+  task1.toDolist = ['eating something', '10:00 am', true];
+  print(task1.toDolist);
+  task1.addTask();
+  task1.removeTask();
+}
+
+class toDoList {
+  List<dynamic>? toDolist;
+  void addTask() {
+    List<dynamic> addTask = ['reading', '5:pm', true];
+    print(addTask);
+  }
+
+  dynamic removeTask() {}
+
+  void updateTaske() {}
+}
